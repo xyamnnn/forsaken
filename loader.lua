294f141a6779b664c8e08257628b4511f7baa604
hussssss <adrian@alicea.us>
2025-06-16 16:56:49 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,27 @@
+local gameScripts = {
+    [18687417158] = "forsaken.lua",
+    -- placeholder
+}
+
+local baseURL = "https://raw.githubusercontent.com/xyamnnn/sillyprojects/main/"
+local Players = game:GetService("Players")
+local plr = Players.LocalPlayer
+
+local scriptFile = gameScripts[game.PlaceId]
+
+if scriptFile then
+    loadstring(game:HttpGet(baseURL .. scriptFile))()
+else
+    plr:Kick("this game is not supported, if this is the right game and ur getting this error report it here to me. https://discord.gg/ZEWJGgsP7e")
+end
+
+for _, conn in next, getconnections(plr.Idled) do
+    conn:Disable()
+end
+
+local VirtualUser = game:GetService("VirtualUser")
+
+plr.Idled:Connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+end)
